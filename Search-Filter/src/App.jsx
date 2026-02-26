c03be90ada9cb07fa99989129337064925e56cd4
NikhilPatil2727 <nikhilpatil02436@gmail.com>
2026-02-26 11:02:14 +0530

    search filtered list completed

--- Search-Filter/src/App.jsx
+++ Search-Filter/src/App.jsx
@@ -1,18 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
- 
+  const [search, setSearch] = useState("");
+
+  const [debounce, setDebounce] = useState("");
+
+  let item = ["apple", "banana", "watermilleon", "orange", "Milk"];
+
+  useEffect(() => {
+    let time = setTimeout(() => {
+      setDebounce(search);
+    }, 1000);
+
+    return () => {
+      clearTimeout(time);
+    };
+  }, [search]);
+
+  const filteredlist=item.filter((data)=>data.toLowerCase().includes(debounce.toLowerCase()))
 
   return (
     <>
-      
-      
-    
-        
+      <div>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+
+      {
+        filteredlist.map((item)=>(
+          <div>
+            {item}
+          </div>
+        ))
+      }
     </>
-  )
+  );
 }
 
-export default App
+export default App;
